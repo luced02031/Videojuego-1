b0e4080a2e7b1a930c8cf33c430a7fc365e518fd
Luis Cedillo M <luisced02031@gmail.com>
2021-01-10 18:38:12 -0600

    window has been created

---
+++ serp.py
@@ -0,0 +1,12 @@
+import turtle as t
+
+# VENTNA
+
+window = t.Screen()
+window.title("Juego prueba")
+window.bgcolor("black")
+window.setup(width=600, height=600)
+window.tracer(0)
+
+# La GUI no se cierra hasta que hayamos picado 'X'
+t.mainloop()
